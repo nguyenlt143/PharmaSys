969f13d28b2329750deb3a4756b20187864d26bf
ngthanhduy2003 <133522438+ngthanhduy2003@users.noreply.github.com>
2025-09-18 09:47:05 +0700

    Add files via upload
    Create folder structure

---
+++ pharmasys/src/main/java/com/g26/pharmasys/PharmasysApplication.java
@@ -0,0 +1,13 @@
+package com.g26.pharmasys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmasysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmasysApplication.class, args);
+	}
+
+}
