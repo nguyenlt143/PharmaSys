77e96bf51a2370443e1cf76eb91332eb470a1364
ngthanhduy2003 <ngthanhduy2003@gmail.com>
2025-09-18 10:06:13 +0700

    Folder structure

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="duy"
+
+ENTRYPOINT ["top", "-b"]
---
+++ src/test/java/com/g26/pharmasys/PharmasysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g26.pharmasys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmasysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
